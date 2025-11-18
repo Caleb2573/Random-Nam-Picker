fc3ea16365ac82e70c52fc876be7c6f0cdd94940
Caleb Yates <caleb.yates@student.swic.edu>
2025-11-17 20:37:47 -0600

    Create light and dark mode buttons

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import "./style.css";
 
 const button = document.querySelector("button");
 const nameDisplay = document.querySelector("p");
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+const body = document.querySelector("body");
 
 button.addEventListener("click", () => {
   nameDisplay.textContent =
     data[getRandomIntInclusive(0, data.length - 1)].name;
+});
+
+darkButton.addEventListener("change", () => {
+  body.classList.remove("bg-gray-100");
+  body.classList.add("bg-gray-800", "text-white");
+});
+
+lightButton.addEventListener("change", () => {
+  body.classList.remove("bg-gray-800", "text-white");
+  body.classList.add("bg-gray-100");
 });
 
 function getRandomIntInclusive(min, max) {
